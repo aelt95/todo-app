42bae3dc1ee5fa952fe3e2bf3f5ea6a80e512822
Anthony Edward Lopez Tovar <anthonylopez1230@gmail.com>
2022-01-19 20:09:25 +0100

    (This commit will) create todo-list & checkbox

--- todo-app/src/components/CreateTodoList.js
+++ todo-app/src/components/CreateTodoList.js
@@ -3,22 +3,28 @@ import { useState, useRef } from "react";
 const CreateTodoList = () => {
   const [isNameList, setIsNameList] = useState(false);
   const [nameList, setNameList] = useState();
+  const [listTodo, setListTodo] = useState([]);
   const nameListInput = useRef();
+  const todoRef = useRef();
 
   const editNameListHandler = () => {
     setIsNameList(false);
   };
 
-  const submitHandler = (e) => {
+  const listNameHandler = (e) => {
     e.preventDefault();
     setNameList(nameListInput.current.value);
     setIsNameList(true);
   };
 
+  const addTodoHandler = (e) => {
+    e.preventDefault();
+    setListTodo([...listTodo, todoRef.current.value]);
+  };
   return (
     <div>
       {!isNameList && (
-        <form onSubmit={submitHandler}>
+        <form onSubmit={listNameHandler}>
           <input
             type="text"
             ref={nameListInput}
@@ -29,8 +35,26 @@ const CreateTodoList = () => {
       )}
       {isNameList && (
         <div>
-          <h1>{nameList}</h1>
-          <button onClick={editNameListHandler}>Edit</button>
+          <div>
+            <h1>{nameList}</h1>
+            <button onClick={editNameListHandler}>Edit</button>
+          </div>
+          <form onSubmit={addTodoHandler}>
+            <input
+              placeholder="Add your todo"
+              ref={todoRef}
+              type="text"
+            ></input>
+            <button>Add</button>
+          </form>
+          {listTodo.map((item) => {
+            return (
+              <div>
+                <input type="checkbox"></input>
+                <h3>{item}</h3>
+              </div>
+            );
+          })}
         </div>
       )}
     </div>
