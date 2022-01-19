60908fe5283d988f84a78613f4503ac8bf67343e
Anthony Edward Lopez Tovar <anthonylopez1230@gmail.com>
2022-01-19 17:23:07 +0100

    (This commit will) create list todo name

--- todo-app/src/components/CreateTodoList.js
+++ todo-app/src/components/CreateTodoList.js
@@ -1,3 +1,40 @@
-const CreateTodoList = () => {};
+import { useState, useRef } from "react";
+
+const CreateTodoList = () => {
+  const [isNameList, setIsNameList] = useState(false);
+  const [nameList, setNameList] = useState();
+  const nameListInput = useRef();
+
+  const editNameListHandler = () => {
+    setIsNameList(false);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setNameList(nameListInput.current.value);
+    setIsNameList(true);
+  };
+
+  return (
+    <div>
+      {!isNameList && (
+        <form onSubmit={submitHandler}>
+          <input
+            type="text"
+            ref={nameListInput}
+            placeholder="Name your list"
+          ></input>
+          <button>Create</button>
+        </form>
+      )}
+      {isNameList && (
+        <div>
+          <h1>{nameList}</h1>
+          <button onClick={editNameListHandler}>Edit</button>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default CreateTodoList;
